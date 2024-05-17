8bab80b50bcea69d996fe8b48d21405b971fb141
Om-anand-0 <om.d3v.21@gmail.com>
2024-05-17 23:18:29 +0530

    initial commit

---
+++ variable.cpp
@@ -0,0 +1,10 @@
+int main()
+{
+    [[maybe_unused]]double pi { 3.14159 };
+    double gravity { 9.8 };
+    double pi { 1.61803 };
+
+    // assume some of the above are used here, some are not
+
+    return 0;
+}
